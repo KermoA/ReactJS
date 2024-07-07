09e9e2f87f52fe09dd2db7c5476615b887ae1291
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2024-07-07 13:48:58 +0300

    React Components

--- demo-project/src/index.js
+++ demo-project/src/index.js
@@ -3,17 +3,48 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+var DisplayEmployeeInfo=(employee)=>{
+  return <div>
+    <h1>Employee Details...</h1>
+    <p>
+      <label>Employee ID : <b>{employee.Id}</b></label>
+    </p>
+    <p>
+      <label>Employee Name : <b>{employee.Name}</b></label>
+    </p>
+    <p>
+      <label>Employee Location : <b>{employee.Location}</b></label>
+    </p>
+    <p>
+      <label>Employee Salary : <b>{employee.Salary}</b></label>
+    </p>
+    <Department deptName={employee.deptName} headName={employee.headName}></Department>
+  </div>;
+}
+
+const Department=(deptInfo)=>{
+  return <div>
+    <p>Dept Name is : <b>{deptInfo.deptName}</b></p>
+    <p>Dept Head is : <b>{deptInfo.headName}</b></p>
+  </div>;
+}
+
+const element=<DisplayEmployeeInfo Id="101" Name="Kermo" Location="Tallinn" Salary="4000" deptName="UI Dev" headName="Kermo Tech"></DisplayEmployeeInfo>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
+
 // const element=React.createElement("h1", null, "Welcome to React Programming World...");
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(element);
 
-const element=React.createElement("div", {className:"testClass"},
-  React.createElement("h1", null, "Welcome to React Programming"),
-  React.createElement("h1", null, "Understanding the Creation of Elements"));
+// const element=React.createElement("div", {className:"testClass"},
+//   React.createElement("h1", null, "Welcome to React Programming"),
+//   React.createElement("h1", null, "Understanding the Creation of Elements"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(element);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(element);
 
 // const element = <h1 className='testClass'>Welcome to React Programming...</h1>;
 // const root = ReactDOM.createRoot(document.getElementById('root'));
