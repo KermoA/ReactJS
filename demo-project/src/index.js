f63edcc5569b9fbf09c734bdd9d069c7734dccdf
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2024-07-07 13:33:40 +0300

    React Elements

--- demo-project/src/index.js
+++ demo-project/src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const element=React.createElement("h1", null, "Welcome to React Programming World...");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(element);
+
+const element=React.createElement("div", {className:"testClass"},
+  React.createElement("h1", null, "Welcome to React Programming"),
+  React.createElement("h1", null, "Understanding the Creation of Elements"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
+
+// const element = <h1 className='testClass'>Welcome to React Programming...</h1>;
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(element);
+
+// const newElement = <h1 className='testClass'>Understanding the Creation of Elements in React...</h1>;
+// const appRoot = document.getElementById('app');
+// if (appRoot) {
+//   const appRootContainer = ReactDOM.createRoot(appRoot);
+//   appRootContainer.render(newElement);
+// }
+
+// const element=(
+//   <div className='testClass'>
+//     <h1>Welcome to React Programming...</h1>
+//     <h1>Understanding the Creation of React Elements...</h1>
+//   </div>
+// );
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(element);
+
 reportWebVitals();
